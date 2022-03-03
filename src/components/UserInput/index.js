557a4dad6230e992ac46d40c0096c5fbd711e9b3
rhobbikhaidir <rhobbie09@gmail.com>
2022-03-03 17:09:15 +0700

    delete varible useless

--- src/components/UserInput/index.js
+++ src/components/UserInput/index.js
@@ -55,9 +55,7 @@ const UserInput = (props) => {
 
       for (j = 0; j < len; j++) {
         if (arr[j] > arr[j + 1]) {
-          let newBubleArr = arr[j];
           arr[j] = arr[j + 1];
-          arr[j + 1] = newBubleArr;
           props.handleBubbleArr(arr);
         }
       }
